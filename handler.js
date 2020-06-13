55d7db19ddef10d20c77484bfb3c0d5e7a86aa3d
helloheatherw <heather.williams@teachfirst.org.uk>
2020-06-13 13:09:47 +0100

    Added post request and created a bit of callback hell

--- handler.js
+++ handler.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
-  host: "...eu-west-1.rds.amazonaws.com",
+  host: "",
   user: "admin",
-  password: "...",
+  password: "",
   database: "tasks"
 })
 
 app.get("/tasks", function(req, res) {
-  const taskDetail = req.query.taskDescription;
   const query = "SELECT * FROM task;"
 
   connection.query(query, function(error, data) {
@@ -30,6 +31,41 @@ app.get("/tasks", function(req, res) {
       })
     }
   });
+});
+
+//Request body will look something like this
+// const body = {
+//   "description": "Tidy up spare room",
+//   "completed": false,
+//   "username": "heather"
+// }
+
+app.post("/tasks", function(req, res) {
+  const query = "INSERT INTO task (description, completed, username) VALUES (?, ?, ?);";
+  const querySelect = "SELECT * FROM task WHERE task_id = ?";
+
+  connection.query(query, [req.body.description, req.body.completed, req.body.username], function(error, data) {
+    if(error) {
+      console.log("Error adding a task", error);
+      res.status(500).json({
+        error: error
+      })
+    } else {
+      connection.query(querySelect, [data.insertId], function(error, data) {
+        if(error) {
+          console.log("Error getting the task", error);
+          res.status(500).json({
+            error: error
+          })
+        } else {
+          res.status(201).json({
+            task: data
+          })
+        }
+      })
+    }
+
+  })
 });
 
 
