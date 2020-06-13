bdaeeb223647f836d79f3ed92d82d8ea710297d8
helloheatherw <heather.williams@teachfirst.org.uk>
2020-06-13 11:35:09 +0100

    Set up connection to DB, complete get function, using dummy data for DB connection

--- handler.js
+++ handler.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 
 app.use(cors());
 
+const connection = mysql.createConnection({
+  host: "...eu-west-1.rds.amazonaws.com",
+  user: "admin",
+  password: "...",
+  database: "tasks"
+})
+
 app.get("/tasks", function(req, res) {
   const taskDetail = req.query.taskDescription;
+  const query = "SELECT * FROM task;"
 
-  res.send({ tasks: ["water plants", "do dishes", "buy oats", taskDetail] });
+  connection.query(query, function(error, data) {
+    if(error) {
+      console.log("Error fetching tasks", error);
+      res.status(500).json({
+        error: error
+      })
+    } else {
+      res.status(200).json({
+        tasks: data
+      })
+    }
+  });
 });
 
 
